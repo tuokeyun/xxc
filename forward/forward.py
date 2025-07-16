63c0f82d335058488f1ef0438eda28f0a9db7d68
tuokeyun <clarisethor4154@gmail.com>
2025-07-16 15:20:59 +0800

    Update forward.py

--- forward/forward.py
+++ forward/forward.py
@@ -2,23 +2,25 @@ import asyncio
 from telethon import TelegramClient
 from forward.forwarder import forward_message_from_redis
 from utils.config import get_config
+from utils.logger import get_logger
 
 # 获取配置
 config = get_config()
 
 # 转发功能
-async def start_forwarding(client, forward_chat_id):
+async def start_forwarding(client, forward_chat_id, session_name):
     """启动转发任务"""
+    logger = get_logger(session_name)
     while True:
         try:
             await forward_message_from_redis(client, forward_chat_id)
         except Exception as e:
-            print(f"[{client.session_name}] ❌ 转发任务异常: {str(e)}")
+            logger.error(f"[{session_name}] ❌ 转发任务异常: {str(e)}")
         await asyncio.sleep(1)
 
 # 启动转发
-async def forward(client, forward_chat_id):
+async def forward(client, forward_chat_id, session_name):
     """启动转发小号任务"""
-    print(f"[{client.session_name}] 🚀 启动转发任务")
-    await start_forwarding(client, forward_chat_id)
-
+    logger = get_logger(session_name)
+    logger.info(f"[{session_name}] 🚀 启动转发任务")
+    await start_forwarding(client, forward_chat_id, session_name)
